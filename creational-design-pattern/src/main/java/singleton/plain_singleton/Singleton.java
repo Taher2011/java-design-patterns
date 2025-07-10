346e0be50eccf4f0046eb1ccbe3346859aa35ad2
Taher <taherc.ali@gmail.com>
2025-07-10 13:52:15 +0530

    singleton pattern

--- creational-design-pattern/src/main/java/singleton/plain_singleton/Singleton.java
+++ creational-design-pattern/src/main/java/singleton/plain_singleton/Singleton.java
@@ -27,6 +27,7 @@ public class Singleton {
 
 	public static void main(String[] args) {
 		Singleton singleton1 = getSingleton();
+		singleton1.setData(55);
 		System.out.println("First reference: " + singleton1.hashCode());
 		System.out.println("Singleton data value is: " + singleton1.getData());
 
---
+++ creational-design-pattern/src/main/java/singleton/reflection_breaking_singleton/Singleton.java
@@ -0,0 +1,50 @@
+package reflection_breaking_singleton;
+
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
+
+public class Singleton {
+
+	private Singleton() {
+	}
+
+	private static Singleton singleton;
+
+	private int data;
+
+	public static Singleton getSingleton() {
+		if (singleton == null) {
+			singleton = new Singleton();
+			return singleton;
+		}
+		return singleton;
+	}
+
+	public int getData() {
+		return data;
+	}
+
+	public void setData(int data) {
+		this.data = data;
+	}
+
+	public static void main(String[] args)
+			throws InstantiationException, IllegalAccessException, IllegalArgumentException, InvocationTargetException {
+		Singleton singleton1 = getSingleton();
+		singleton1.setData(55);
+		System.out.println("First reference: " + singleton1.hashCode());
+		System.out.println("Singleton data value is: " + singleton1.getData());
+
+		System.out.println();
+
+		Constructor<?>[] declaredConstructors = Singleton.class.getDeclaredConstructors();
+		for (Constructor<?> constructor : declaredConstructors) {
+			constructor.setAccessible(true);
+			Singleton reflectionedSingleton = (Singleton) constructor.newInstance();
+			System.out.println("Second reference: " + reflectionedSingleton.hashCode());
+			System.out.println("Singleton data value is: " + reflectionedSingleton.getData());
+		}
+
+	}
+
+}

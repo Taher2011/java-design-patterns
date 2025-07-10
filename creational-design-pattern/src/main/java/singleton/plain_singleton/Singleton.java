dcf29d4066aea56d705b563e705533c46f0198b4
Taher <taherc.ali@gmail.com>
2025-07-10 09:13:17 +0530

    singleton pattern

--- creational-design-pattern/src/main/java/singleton/plain_singleton/Singleton.java
+++ creational-design-pattern/src/main/java/singleton/plain_singleton/Singleton.java
@@ -9,13 +9,18 @@ public class Singleton {
 
 	public static Singleton getSingleton() {
 		if (singleton == null) {
-			return new Singleton();
+			singleton = new Singleton();
+			return singleton;
 		}
 		return singleton;
 	}
 
 	public static void main(String[] args) {
+		Singleton singleton1 = getSingleton();
+		System.out.println(singleton1.hashCode());
 
+		Singleton singleton2 = getSingleton();
+		System.out.println(singleton2.hashCode());
 	}
 
 }

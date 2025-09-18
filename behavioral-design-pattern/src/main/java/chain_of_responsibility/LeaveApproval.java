76687417ac3b5f63f09773830a45fcd1052eaa07
Taher <taherc.ali@gmail.com>
2025-09-18 07:41:51 +0530

    restructure

--- behavioral-design-pattern/src/main/java/chain_of_responsibility/LeaveApproval.java
+++ behavioral-design-pattern/src/main/java/chain_of_responsibility/LeaveApproval.java
@@ -22,21 +22,10 @@ class LeaveApplication {
 @Getter
 abstract class Employeee {
 
-	private String roleName;
-
 	private Employeee successor;
 
-	protected Employeee(String roleName, Employeee successor) {
-		this.roleName = roleName;
+	protected Employeee(Employeee successor) {
 		this.successor = successor;
-	}
-
-	public boolean approveLeaveApplicationRequest(LeaveApplication leaveApplication) {
-		if (!handleRequest(leaveApplication) && successor != null) {
-			successor.approveLeaveApplicationRequest(leaveApplication);
-		}
-		System.out.println();
-		return false;
 	}
 
 	protected abstract boolean handleRequest(LeaveApplication leaveApplication);
@@ -46,21 +35,20 @@ abstract class Employeee {
 // concrete handler
 class LeadEmployeeHandler extends Employeee {
 
-	public LeadEmployeeHandler(String roleName, Employeee successor) {
-		super(roleName, successor);
+	public LeadEmployeeHandler(Employeee successor) {
+		super(successor);
 	}
 
 	@Override
 	public boolean handleRequest(LeaveApplication leaveApplication) {
 		long between = Duration.between(leaveApplication.getFrom(), leaveApplication.getTo()).toDays();
 		if (leaveApplication.getType().equals("Sick") && between <= 2) {
-			leaveApplication.setApprover(getRoleName());
+			leaveApplication.setApprover("Lead");
 			System.out.println("request approved by " + leaveApplication.getApprover());
 			return true;
 		} else {
-			this.getSuccessor().handleRequest(leaveApplication);
+			return this.getSuccessor().handleRequest(leaveApplication);
 		}
-		return false;
 	}
 
 }
@@ -68,21 +56,20 @@ class LeadEmployeeHandler extends Employeee {
 //concrete handler
 class ManagerEmployeeHandler extends Employeee {
 
-	public ManagerEmployeeHandler(String roleName, Employeee successor) {
-		super(roleName, successor);
+	public ManagerEmployeeHandler(Employeee successor) {
+		super(successor);
 	}
 
 	@Override
 	public boolean handleRequest(LeaveApplication leaveApplication) {
 		long between = Duration.between(leaveApplication.getFrom(), leaveApplication.getTo()).toDays();
 		if ((leaveApplication.getType().equals("Sick") || leaveApplication.getType().equals("PTO")) && between <= 5) {
-			leaveApplication.setApprover(getRoleName());
+			leaveApplication.setApprover("Manager");
 			System.out.println("request approved by " + leaveApplication.getApprover());
 			return true;
 		} else {
-			this.getSuccessor().handleRequest(leaveApplication);
+			return this.getSuccessor().handleRequest(leaveApplication);
 		}
-		return false;
 	}
 
 }
@@ -90,15 +77,15 @@ class ManagerEmployeeHandler extends Employeee {
 //concrete handler
 class DirectorEmployeeHandler extends Employeee {
 
-	public DirectorEmployeeHandler(String roleName, Employeee successor) {
-		super(roleName, successor);
+	public DirectorEmployeeHandler(Employeee successor) {
+		super(successor);
 	}
 
 	@Override
 	public boolean handleRequest(LeaveApplication leaveApplication) {
 		long between = Duration.between(leaveApplication.getFrom(), leaveApplication.getTo()).toDays();
 		if ((leaveApplication.getType().equals("PTO") || leaveApplication.getType().equals("LWP")) && between <= 10) {
-			leaveApplication.setApprover(getRoleName());
+			leaveApplication.setApprover("Director");
 			System.out.print("request approved by " + leaveApplication.getApprover());
 			return true;
 		}
@@ -113,9 +100,9 @@ public class LeaveApproval {
 		LeaveApplication leaveApplication = LeaveApplication.builder().type("LWP").from(LocalDateTime.now())
 				.to(LocalDateTime.of(2025, 07, 23, 0, 0)).build();
 
-		Employeee directroHandler = new DirectorEmployeeHandler("Director", null);
-		Employeee managerHandler = new ManagerEmployeeHandler("Manager", directroHandler);
-		Employeee leadHandler = new LeadEmployeeHandler("Lead", managerHandler);
+		Employeee directroHandler = new DirectorEmployeeHandler(null);
+		Employeee managerHandler = new ManagerEmployeeHandler(directroHandler);
+		Employeee leadHandler = new LeadEmployeeHandler(managerHandler);
 		leadHandler.handleRequest(leaveApplication);
 		System.out.print(" for leave type " + leaveApplication.getType() + " for "
 				+ Duration.between(leaveApplication.getFrom(), leaveApplication.getTo()).toDays() + " days");

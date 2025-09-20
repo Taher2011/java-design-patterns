829ca166481a8a82be85ef92fe00c64e3212ec29
Taher <taherc.ali@gmail.com>
2025-09-20 12:27:25 +0530

    restructure

--- creational-design-pattern/src/main/java/abstract_factory/vehicle/_01_product/AsianVehicle.java
+++
@@ -1,4 +0,0 @@
-package abstract_factory.vehicle._01_product;
-
-public abstract class AsianVehicle extends Vehicle {
-}
---
+++ creational-design-pattern/src/main/java/abstract_factory/vehicle/_01_product/Hatchback.java
@@ -0,0 +1,4 @@
+package abstract_factory.vehicle._01_product;
+
+public abstract class Hatchback extends Vehicle {
+}
